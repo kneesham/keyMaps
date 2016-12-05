54464cc05aad78e912c26f5462c7d23eb284dd20
Kneesham <vlog.car.guy@gmail.com>
2016-12-04 21:04:43 -0600

    working a little better here

--- jsBin/Keymaps.js
+++ jsBin/Keymaps.js
@@ -1,8 +1,6 @@
+
+
 function getLetter(event) {
-  // var x = event.which || event.keyCode;
-  //
-  // document.getElementById("letter").innerHTML = "the number is: " + x;
-  //
 
   var alphabet = "";
   for( var i=0; i<26; i++)
