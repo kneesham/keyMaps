aa0d804ebb81ba010c8083e839e864bd8fb68858
Kneesham <vlog.car.guy@gmail.com>
2016-12-04 20:41:38 -0600

    Keymaps now prints out the alphabet from a instead of giving you the char code number.

--- jsBin/Keymaps.js
+++ jsBin/Keymaps.js
@@ -1,8 +1,16 @@
 function getLetter(event) {
-  var x = event.which || event.keyCode;
+  // var x = event.which || event.keyCode;
+  //
+  // document.getElementById("letter").innerHTML = "the number is: " + x;
+  //
 
-  document.getElementById("letter").innerHTML = "the number is: " + x;
+  var alphabet = "";
+  for( var i=0; i<26; i++)
+  {
 
+     alphabet += String.fromCharCode(97 + i);
+  }
+  document.getElementById("letter").innerHTML = "the alphabet is: " + alphabet;
 
 
 }
